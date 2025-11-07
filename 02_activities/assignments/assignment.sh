cfc6f27b9174a0e67cea389054a6517f213811d1
Nicole Latendresse <NicoleLatendresse@users.noreply.github.com>
2025-11-06 22:49:44 -0500

    Corrected error - used mv instead of cp

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,11 +44,11 @@ mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ../raw/rawdata
-mv *server*.log ../../processed/server_logs
+cp *server*.log ../../processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-mv *user*.log ../../processed/user_logs
-mv *event*.log ../../processed/event_logs
+cp *user*.log ../../processed/user_logs
+cp *event*.log ../../processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm *ipaddr* 
